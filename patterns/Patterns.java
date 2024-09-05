f3fb2327e05e5fd1bf681f2da2c2b46b1b922918
RSarvjyoti <sarvjyoti64@gmail.com>
2024-09-05 10:22:36 +0530

    18 patterns

--- patterns/Patterns.java
+++ patterns/Patterns.java
@@ -22,6 +22,24 @@ public class Patterns {
         p8(n);
         System.out.println();
         p9(n);
+        System.out.println();
+        p10(n);
+        System.out.println();
+        p11(n);;
+        System.out.println();
+        p12(n);
+        System.out.println();
+        p13(n);
+        System.out.println();
+        p14(n);
+        System.out.println();
+        p15(n);
+        System.out.println();
+        p16(n);
+        System.out.println();
+        p17(n);
+        System.out.println();
+        p18(n);
     }
 
     static void p1(int n ) {
@@ -128,6 +146,149 @@ public class Patterns {
     static void p9(int n) {
         p7(n);
         p8(n);
+    }
+
+    static void p10(int n ) {
+
+        for(int i =1; i<=2*n - 1; i++) {
+
+            // star
+            int star = i > n ? 2 * n - i : i;
+
+            for(int j =1; j<=star; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+
+    }
+
+    static void p11(int n) {
+        
+        for(int i=0; i<n; i++) {
+
+            for(int j =0; j<=i; j++) {
+                if(j%2 == i%2) {
+                    System.out.print(1 + " ");
+                }else{
+                    System.out.print(0 + " ");
+                }
+            }
+
+            System.out.println();
+
+        }
+    }
+
+    static void p12(int n) {
+        for(int i=1; i<=n; i++) {
+            // numbers
+            for(int j=1; j<=i; j++) {
+                System.out.print(j);
+            }
+            // space
+            for(int j=1; j<=(2*n - 2*i); j++) {
+                System.out.print(" ");
+            } 
+            // numbers
+            for(int j=1; j<=i; j++) {
+                System.out.print(i-j+1);
+            }
+
+            System.out.println();
+        }
+    }
+
+    static void p13(int n) {
+        int num = 1;
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=i; j++) {
+                System.out.print(num + " ");
+                num++;
+            }
+            System.out.println();
+        }
+    }
+
+    static void p14(int n) {
+        for(int i=0; i<n; i++) {
+            for(char ch = 'A'; ch<='A' + i; ch++) {
+                System.out.print(ch + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void p15(int n) {
+        for(int i=0; i<n; i++) {
+            for(char ch = 'A'; ch<='A' + n - i - 1; ch++) {
+                System.out.print(ch + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void p16(int n) {
+       for(int i=0; i<n; i++) {
+        char ch = (char)('A' + i);
+        for(int j=0; j<=i; j++) {
+            System.out.print(ch + " ");
+        }
+        System.out.println();
+       }
+    }
+
+    static void p17(int n) {
+        for(int i=0; i<n; i++) {
+            // spaces
+            for(int j =0; j<n - i - 1; j++) {
+                System.out.print(" ");
+            }
+
+            // characters
+            char ch = 'A';
+            int breakPoint = (2*i+1)/2;
+            for(int j=1;j<=2*i+1; j++) {
+                System.out.print(ch);
+                if(j <= breakPoint) {
+                    ch++;
+                }else{
+                    ch--;
+                }
+    
+            }
+            // spaces
+            for(int j =0; j<n - i - 1; j++) {
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void p18(int n) {
+        for(int i=0; i<n; i++) {
+            for(char ch = (char)('E' - i); ch <='E'; ch++) {
+                System.out.print(ch+ " ");
+            }
+            System.out.println();
+        }
+    }
+
+    static void p19(int n) {
+        
+    }
+
+    static void p20(int n) {
+
+    }
+
+    static void p21(int n) {
+
+    }
+
+    static void p22(int n) {
+
     }
 
 }
