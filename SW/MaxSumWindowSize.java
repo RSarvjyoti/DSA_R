b5af9d65067cad20a4b599d0083b94dbe332683d
RSarvjyoti <sarvjyoti64@gmail.com>
2025-03-11 11:09:41 +0530

    type 1

--- SW/MaxSumWindowSize.java
+++ SW/MaxSumWindowSize.java
@@ -2,10 +2,14 @@ class MaxSumWindowSize{
     public static void main(String[] args) {
         int arr[] = {1,2,3,4,5,6,7,8};
         int k = 3;
-        System.out.println(maxSumSizeK(arr, k));
+        System.out.println(aproach1(arr, k));
+        System.out.println(aproach2(arr, k));
     }
     
-    static int maxSumSizeK(int arr[], int k){
+    // TC = O(n.k),  SC = O(1)
+    // - Outer loop will going i = 0 to i < n - k + 1
+    // - Inner loop will going j = i to j < i + k
+    static int aproach1(int arr[], int k){
         int n = arr.length;
         int max = Integer.MIN_VALUE;
         for(int i = 0; i<n-k+1; i++){
@@ -15,6 +19,25 @@ class MaxSumWindowSize{
             }
             max = Math.max(sum, max);
         }
+        return max;
+    }
+    // SW aproach : 
+    // - TC = O(n), SC = O(1);
+    static int aproach2(int arr[], int k){
+        int n = arr.length;
+        int max = Integer.MIN_VALUE;
+        int sum = 0;
+        for(int i = 0 ; i<k; i++) {
+            sum += arr[i];
+        }
+
+        max = sum;
+        for(int i = k; i < n; i++){
+            sum += arr[i] - arr[i- k];
+            max = Math.max(max, sum);
+        }
+
+
         return max;
     }
 }
--- SW/Readme.md
+++ SW/Readme.md
@@ -2,7 +2,5 @@
 They are 2 types 
 1. Given a window size
  - No. of subarray = (n - k + 1), where n = size of array, k = given size of window
- - Outer loop will going i = 0 to i < n - k + 1
- - Inner loop will going j = i to j < i + k
 2. Variable size of window
  - No. of subarray = n * (n + 1) / 2
